685ac6a7ffc369bcdb320cb43b7d43a1a0d1bacd
soren <sorenessen@gmail.com>
2016-08-06 13:59:10 -0400

    + arrays and maps

---
+++ src/Project6/MultiDimensionArray.java
@@ -0,0 +1,29 @@
+package Project6;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ * Created by soren on 8/6/2016.
+ */
+public class MultiDimensionArray {
+    public static void main(String[] args) {
+        System.out.println("Please enter numbers: ");
+       Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        int rows = 300;
+        int column = 300;
+        int [][] multiArray = new int[rows][column];
+        for(int i=0; i<rows; i++){
+            for (int j=0; j<column; j++){
+                multiArray[i][j] = random.nextInt(100);
+            }
+        }
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < column; j++) {
+                System.out.print(multiArray[i][j]);
+                System.out.print(" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Project6/UseListMap.java
@@ -0,0 +1,52 @@
+package Project6;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Iterator;
+/**
+ * Created by soren on 8/6/2016.
+ */
+public class UseListMap {
+    public static void main(String[] args) {
+        List<String> citiesOfUSA = new ArrayList<String>();
+        citiesOfUSA.add("NY");
+        citiesOfUSA.add("CO");
+        citiesOfUSA.add("SF");
+
+        List<String> citiesOfCanada = new ArrayList<String>();
+        citiesOfCanada.add("Ontario");
+        citiesOfCanada.add("Montreal");
+        citiesOfCanada.add("Nova Scotia");
+
+        List<String> citiesOfBangladesh = new ArrayList<String>();
+        citiesOfBangladesh.add("Dhaka");
+        citiesOfBangladesh.add("Comilla");
+        citiesOfBangladesh.add("Sylhet");
+
+        List<String> citiesOfUK = new ArrayList<String>();
+        citiesOfUK.add("London");
+        citiesOfUK.add("Kent");
+        citiesOfUK.add("Manchester");
+
+        Map<String, List<String>> map = new LinkedHashMap<String, List<String>>();
+        map.put("USA", citiesOfUSA);
+        map.put("UK", citiesOfUK);
+        map.put("Canada", citiesOfCanada);
+        map.put("Bangladesh", citiesOfBangladesh);
+
+        for(Map.Entry<String, List<String>> entry:map.entrySet()){
+            System.out.println(entry.getKey()+ " : " + entry.getValue());
+
+        }
+
+        System.out.println("....Retrieve the values by Iterator and While Loop...");
+        Iterator it = map.entrySet().iterator();
+            while(it.hasNext()){
+                Map.Entry data = (Map.Entry)it.next();
+                System.out.println(data.getKey() + " " + data.getValue());
+            }
+
+    }
+
+}
